3093eacc0a74dd70195441da33195260ba50af5e
yona l <04l.yona@gmail.com>
2018-01-21 18:33:10 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,12 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('div img:last')
+}
+
+function divSelector(){
+  return $();
+}
